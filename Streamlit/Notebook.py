bff2597a42ba11faaaf7884f6ca1eff1597173c0
Mijan <mijansamuel@gmail.com>
2022-07-18 14:05:15 +0100

    Function_Edit

--- Streamlit/Notebook.py
+++ Streamlit/Notebook.py
@@ -1,88 +1,58 @@
-#!/usr/bin/env python
-# coding: utf-8
+import tensorflow
 
-# In[16]:
-
-
-import pickle
-from tensorflow import keras
-from tensorflow.keras.preprocessing import image
-import matplotlib.pyplot as plt
-get_ipython().run_line_magic('matplotlib', 'inline')
+import keras
+from PIL import Image, ImageOps
 import numpy as np
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Activation, Dense, Flatten, BatchNormalization, Conv2D, MaxPool2D
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.metrics import categorical_crossentropy
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from sklearn.metrics import confusion_matrix
+import itertools
+import os
+import shutil
+import random
+import glob
+import matplotlib.pyplot as plt
+from tensorflow.keras.models import Sequential, model_from_json
+import tensorflow_hub as hub
 
 
-# In[2]:
 
-
-pickled_model = pickle.load(open('model.pkl', 'rb'))
-
-
-# In[10]:
-
-
-MODEL = keras.models.load_model("model.h5")
-
-
-# In[7]:
-
-
-img = image.load_img("images/bak_img5.jpg")
-
-
-# In[56]:
-
-
-dir_path = "images/healthy_img.jpg"
-
-
-img=image.load_img(dir_path , target_size=(224,224))
-
-    
-X = image.img_to_array(img)
-X =np.expand_dims(X,axis=0)
-
-    
-classes = model.predict(X)
-# if classes == 0:
-#     print("You look Happy \n You must be having a Good day!")
-# else:
-#     print('You Do not Look Happy \n Is Everything Okay?')
+def teachable_model(img, weight_file):
+    #Load Json File, Model Architecture
+    json_file = open('mobile_net_model.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+   
+    # Load Model Weights
+    loaded_model = model_from_json(loaded_model_json,custom_objects={'KerasLayer':hub.KerasLayer})
+    loaded_model.load_weights(weight_file, by_name=True)
     
 
+    # Create the array of the right shape to feed into the keras model
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+   
+    #image sizing
+    size = (224, 224)
+    image = ImageOps.fit(img, size, Image.ANTIALIAS)
 
-# In[57]:
+    #turn the image into a numpy array
+    image_array = np.asarray(image)
+    X =np.expand_dims(image_array,axis=0)
 
-
-out = classes.tolist()
-
-
-# In[58]:
-
-
-for i in classes:
-    print(i)
-    for n in i:
-        print(n)
+    # Normalize the image
+    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+    # Model target classes in a dictionary
+    classes = {0:'Tomatoes Bacterial spot', 1:'Tomatoes Early blight', 2:'Tomatoes Late blight', 
+               3:'Tomatoes Leaf Mold', 4:'Tomatoes Septoria leaf spot', 
+               5:'Tomatoes Spider mites Two-spotted spider mite', 6:'Tomatoes Target Spot',
+               7:'Tomatoes Tomato Yellow Leaf Curl Virus',8:'Tomatoes Tomato mosaic virus',
+               9:'Healthy Tomatoe Leaf'}
     
-
-
-# In[55]:
-
-
-img
-
-
-# In[ ]:
-
-def predictions(input_image):
-
-    img=image.load_img(input_image , target_size=(224,224))
-
-    X = image.img_to_array(img)
-    X =np.expand_dims(X,axis=0)
-    
-    classes = MODEL.predict(X)
-    return classes
-
-
+    prediction = loaded_model.predict(X)
+    return classes[np.argmax(prediction)] # return position of the highest probability
